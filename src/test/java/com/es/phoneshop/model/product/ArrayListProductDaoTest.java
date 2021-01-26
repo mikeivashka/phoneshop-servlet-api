0492620a404c8730a591cbf49296db9afaa840e3
Mikhail Ivashka <mike.ivashka@gmail.com>
2021-01-26 20:36:46 +0300

    Implement first lecture task ArrayListProductDao implemented Tests implemented

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,21 +1,50 @@
 package com.es.phoneshop.model.product;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
 
-public class ArrayListProductDaoTest
-{
+public class ArrayListProductDaoTest extends Assert {
     private ProductDao productDao;
+    private Product product;
 
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+        product = new Product("simsxg76", "Siemens SXG75", new BigDecimal(150), Currency.getInstance("USD"), 40, "sampleURL");
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void testGetProduct() {
+        Product productToGet = productDao.findProducts().get(0);
+        assertEquals(productToGet, productDao.getProduct(productToGet.getId()).get());
+    }
+
+    @Test
+    public void testFindProductsNotEmpty() {
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Long productToDeleteId = productDao.findProducts().get(0).getId();
+        productDao.delete(productToDeleteId);
+        assertFalse(productDao.getProduct(productToDeleteId).isPresent());
+    }
+
+    @Test
+    public void testSaveProduct() {
+        productDao.save(product);
+        assertTrue(productDao.findProducts().contains(product));
+    }
+
+    @Test
+    public void testProductGetsIdAfterSave() {
+        assertNull(product.getId());
+        productDao.save(product);
+        assertNotNull(product.getId());
     }
 }
--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -7,28 +7,31 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductListPageServletTest {
+    private final ProductListPageServlet servlet = new ProductListPageServlet();
     @Mock
     private HttpServletRequest request;
     @Mock
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
-
-    private ProductListPageServlet servlet = new ProductListPageServlet();
+    @Mock
+    private ServletConfig servletConfig;
 
     @Before
-    public void setup(){
+    public void setup() throws ServletException {
+        servlet.init(servletConfig);
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
     }
 
@@ -37,5 +40,12 @@ public class ProductListPageServletTest {
         servlet.doGet(request, response);
 
         verify(requestDispatcher).forward(request, response);
+    }
+
+    @Test
+    public void testRequestContainsProducts() throws ServletException, IOException {
+        servlet.doGet(request, response);
+
+        verify(request).setAttribute(eq("products"), anyList());
     }
 }
